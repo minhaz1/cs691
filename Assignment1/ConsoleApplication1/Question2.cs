957fda266b3a1613b24e921b0b4b11804651813b
Minhaz Mahmud <minhazm@gmail.com>
2014-02-23 18:31:06 -0500

    done part 1, started part 2

---
+++ Assignment1/ConsoleApplication1/Question2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    public class Querier
+    {
+
+        //IEnumerable<>
+
+        public Querier(IEnumerable<object> collection)
+        {
+
+        }
+
+        public int CountByProperty(string propertyName, string propertyValue)
+        {
+
+            return 0;
+        }
+
+        public int SumByProperty(string propertyName1, string propertyValue1, string propertyName2, string propertyValue2)
+        {
+               
+            return 0;
+        }
+
+        public int GroupByProperty(string propertyName)
+        {
+            return 0;
+        }
+
+        public int Transform(string functionName)
+        {
+
+            return 0;
+        }
+
+
+
+    }
+
+
+
+    class Question2
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
